8ce7a5d7c29d66974b03baf86413b7af9045cb4e
Shiva Eslami <shivaslami@gmail.com>
2023-03-26 10:19:56 +0330

    add Pinia for state management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["@/assets/css/main.css"],
-  modules: ["@nuxt/image-edge"],
+  modules: ["@nuxt/image-edge", "nuxt-icon", "@pinia/nuxt"],
   postcss: {
     plugins: {
       tailwindcss: {},
