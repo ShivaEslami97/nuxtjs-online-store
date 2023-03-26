35c8fc8b849049485993bfd4f6d788f985ec6191
Shiva Eslami <shivaslami@gmail.com>
2023-03-26 10:22:05 +0330

    add default layout

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ProductType } from "~~/types";
+
+useHead({
+  title: "Shopify",
+});
+
+// const productData = useProductStore();
+
+// const { data, pending, error, refresh } = await useAsyncData<ProductType[]>(
+//   "products",
+//   () => $fetch("https://fakestoreapi.com/products")
+// );
+
+// if (data.value && !error.value) {
+//   productData.$patch((state) => {
+//     state.products = data.value as ProductType[];
+//   });
+// }
+</script>
+
+<template>
+  <div>
+    <!-- HOME HERO -->
+    <HomeHero />
+    <!-- HOME PRODUCTS-->
+    <ProductCardsContainer />
+  </div>
+</template>
