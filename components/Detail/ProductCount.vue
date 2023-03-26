7ff02c11cff5abf0ba5f3386dcf92dc2b90f8b94
Shiva Eslami <shivaslami@gmail.com>
2023-03-26 10:35:08 +0330

    feat: add product detail count

---
+++ components/Detail/ProductCount.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+interface Props {
+  count: number;
+}
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div class="mt-3">
+    <p>
+      ONLY
+      <span class="text-primary">{{ count }} </span>
+      LEFT IN STOCK!
+    </p>
+  </div>
+</template>
