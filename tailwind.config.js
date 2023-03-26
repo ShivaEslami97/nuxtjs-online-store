7f048fd8a5ebf8b2f3273e3a1a98c327371b7c0f
Shiva Eslami <shivaslami@gmail.com>
2023-03-26 16:12:28 +0330

    feat: add custom styles to Tailwind CSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,14 +16,19 @@ module.exports = {
         navDrk: "rgb(1 4 10 / 93%)",
         primary: "#ce9a70",
         primaryRgb: "rgb(181 116 64 / 70%)",
+        primaryDrk: "rgb(181 116 64)",
         gray: "#797878",
         grayLtr: "#e7e7e7",
+        overlay: "rgb(0 0 0 / 33%)",
       },
       gridTemplateColumns: {
         // Complex site-specific column configuration
         card: "repeat(auto-fit, minmax(250px, 1fr))",
         detail: "repeat(auto-fit, minmax(33.5rem, 1fr))",
         twoCol: "100px 200px",
+      },
+      boxShadow: {
+        modal: "0 0.5rem 1rem rgb(0 0 0 / 54%)",
       },
     },
   },
