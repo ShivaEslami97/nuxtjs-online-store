f8c4b71c7e7b908541fbaf7689e3eea4e4f04dce
Shiva Eslami <shivaslami@gmail.com>
2023-03-26 10:30:20 +0330

    refactor: Clean up unused code

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,7 @@
 <script setup lang="ts">
-import { ProductType } from "~~/types";
-
 useHead({
   title: "Shopify",
 });
-
-// const productData = useProductStore();
-
-// const { data, pending, error, refresh } = await useAsyncData<ProductType[]>(
-//   "products",
-//   () => $fetch("https://fakestoreapi.com/products")
-// );
-
-// if (data.value && !error.value) {
-//   productData.$patch((state) => {
-//     state.products = data.value as ProductType[];
-//   });
-// }
 </script>
 
 <template>
