ec6ed396b74b697d784ab7f95f7a9a3dfb6e1d38
Shiva Eslami <shivaslami@gmail.com>
2023-03-26 10:33:26 +0330

    feat: add product detail description

---
+++ components/Detail/ProductDescription.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+interface Props {
+  description: string;
+}
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div class="border-t pt-3 border-grayLtr mt-3">
+    <p class="text-gray">{{ description }}</p>
+  </div>
+</template>
