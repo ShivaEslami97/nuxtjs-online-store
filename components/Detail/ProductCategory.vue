dd46c8c5d97824ee0cc358dbecaccffbf037c3e6
Shiva Eslami <shivaslami@gmail.com>
2023-03-26 10:33:58 +0330

    feat: add product detail category

---
+++ components/Detail/ProductCategory.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+interface Props {
+  category: string;
+}
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div class="border-t pt-3 border-grayLtr my-3">
+    <p>
+      Category:
+      <span class="text-gray cursor-pointer">{{ category }}</span>
+    </p>
+  </div>
+</template>
