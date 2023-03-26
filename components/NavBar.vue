418f8c00bc4b047f7082521893cb1cc0796a7312
Shiva Eslami <shivaslami@gmail.com>
2023-03-26 10:20:55 +0330

    add navbar component

---
+++ components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- NAVBAR -->
+  <header class="nav">
+    <div class="container mx-auto">
+      <ul class="flex justify-between items-center z-50 relative">
+        <li><NuxtLink class="text-3xl font-mono" to="/">Shopify</NuxtLink></li>
+        <li>
+          <button>
+            <Icon name="material-symbols:shopping-cart" size="1.6em" />
+          </button>
+        </li>
+      </ul>
+    </div>
+  </header>
+  <!-- NAVBAR -->
+</template>
