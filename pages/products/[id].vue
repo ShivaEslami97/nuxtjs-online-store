c68a0023ccfa478fb8c97cf8ed10b26ad7b20c38
Shiva Eslami <shivaslami@gmail.com>
2023-03-26 10:26:57 +0330

    feat: add product detail page

---
+++ pages/products/[id].vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { useProductStore } from "~~/stores/product";
+
+const route = useRoute();
+const productDetail = useProductStore();
+const { product, error } = storeToRefs(productDetail);
+
+const id = route.params.id;
+
+useHead({
+  title: `Product ${id}`,
+});
+
+const result = await productDetail.getProductsData(
+  `https://fakestoreapi.com/products/${id}`
+);
+console.log(productDetail.error);
+</script>
+
+<template>
+  <DetailProductHero :category="product.category" :title="product.title" />
+  <div class="container mx-auto px-4 my-12">
+    <div class="grid gap-12 grid-cols-detail" v-if="!error">
+      <NuxtImg
+        :src="product.image"
+        class="w-1/2 mx-auto"
+        :alt="product.title"
+      />
+      <div>
+        <DetailProductAttributes
+          :title="product.title"
+          :rate="product.rating.rate"
+          :price="product.price"
+        />
+        <DetailProductDescription :description="product.description" />
+        <DetailProductCount :count="product.rating.count" />
+        <DetailProductAction />
+        <DetailProductCategory :category="product.category" />
+      </div>
+    </div>
+    <h2 class="text-xl py-4" v-else>
+      {{ error }}
+    </h2>
+  </div>
+</template>
