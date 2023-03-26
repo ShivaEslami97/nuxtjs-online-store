0293e7db65a7894abfe914e40bc24e2cf959dbe7
Shiva Eslami <shivaslami@gmail.com>
2023-03-26 10:26:18 +0330

    update tailwindcss config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,22 @@ module.exports = {
     "./error.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        nav: "rgb(0 6 16 / 41%)",
+        navDrk: "rgb(1 4 10 / 93%)",
+        primary: "#ce9a70",
+        primaryRgb: "rgb(181 116 64 / 70%)",
+        gray: "#797878",
+        grayLtr: "#e7e7e7",
+      },
+      gridTemplateColumns: {
+        // Complex site-specific column configuration
+        card: "repeat(auto-fit, minmax(250px, 1fr))",
+        detail: "repeat(auto-fit, minmax(33.5rem, 1fr))",
+        twoCol: "100px 200px",
+      },
+    },
   },
   plugins: [],
 };
