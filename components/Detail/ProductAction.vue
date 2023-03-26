89976c7a570cb2a20c59dea544d1695f3c9c4ef8
Shiva Eslami <shivaslami@gmail.com>
2023-03-26 10:35:28 +0330

    feat: add product detail action

---
+++ components/Detail/ProductAction.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mt-3">
+    <div class="grid grid-cols-twoCol gap-3 mt-3">
+      <input
+        class="border-2 border-[#e7e7e7] rounded text-center focus:outline-none"
+        value="1"
+        type="number"
+        min="1"
+      />
+      <button class="bg-primary rounded px-2 py-2">Add to Cart</button>
+    </div>
+  </div>
+</template>
